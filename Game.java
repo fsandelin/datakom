0b5aeb05f3a8390d29489e9d2257fe3eb4705819
Sandelin <fredriksandelin94@gmail.com>
2016-11-01 03:08:42 +0100

    Added JFrame and canvas as start

--- Game.java
+++ Game.java
@@ -3,13 +3,37 @@
  *
  * @author Fredrik Sandelin
  */
+import java.awt.*;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+import java.awt.Graphics;
+import javax.swing.JFrame;
 
 public class Game {
     public static void main (String [] args) {
-	for (int i = 0; i < 10; i++) {
-	    System.out.println("Hellow World!");
-	}
-	System.out.println("Good bye cruel world!");
-	System.exit(0);
+	
+	int exit = 0;
+	Color grey_color = new Color(105,105,105);
+
+
+	
+	JFrame screen = new JFrame();
+	screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	screen.setSize(800, 600);
+	screen.setTitle("Game");
+
+	//screen.setIgnoreRepaint(true);
+	Canvas canvas = new Canvas();
+	canvas.setIgnoreRepaint(true);
+	canvas.setSize(800,600);
+	canvas.setBackground(grey_color);
+	    
+	screen.add(canvas);
+	screen.pack();
+	screen.setVisible(true);
+
+	while (true) {
+	    canvas.repaint();
+	}	
     }
 }
