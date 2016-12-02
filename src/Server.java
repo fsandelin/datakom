b680b374ad5d50f1e8b9d44025f1de3915c65882
Sandelin <fredriksandelin94@gmail.com>
2016-12-02 14:30:42 +0100

    debug

--- src/Server.java
+++ src/Server.java
@@ -6,4 +6,5 @@ import java.rmi.*;
 public interface Server extends Remote{
     int[] getState() throws RemoteException;
     int connectToGame(String ip, int port, String alias) throws RemoteException;
+    void debug();
 }
